ef7dc29da553821ef115b162336d050fd04475c7
RonaHalili <rh70625@ubt-uni.net>
2025-12-11 00:36:27 +0100

    Ndryshime lokale te app.js

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,17 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+require("./modules/orders/AssociationsOrder");
+
+
+sequelize.sync({ alter: true }) 
+  .then(() => console.log("📌 Tables synced successfully"))
+  .catch(err => console.error("❌ Sync error:", err));
+
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+});
+
 sequelize.authenticate()
   .then(() => console.log("Database connected successfully"))
   .catch(err => console.log("Database connection failed:", err));
